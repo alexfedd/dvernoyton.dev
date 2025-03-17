6a8d3eb4e0cefbe6cd65acf80b6ca4a062bb03d8
alexfedd <alexandrfedorov508@gmail.com>
2025-03-17 21:36:23 +0300

    fixes

--- wp-content/themes/dvernoyton/single-product.php
+++ wp-content/themes/dvernoyton/single-product.php
@@ -154,6 +154,7 @@ $term = array_shift( $terms );
             </div>
           </div>
           <?php endif; ?>
+          <?echo $term->slug ?>
           <?php if ($term->slug == 'composite'):  ?>
             <div class="product-banner__selection">
               <p class="product-banner__text">Серия:</p>
